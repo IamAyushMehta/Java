c93bf79449cc2812de6d36ced7271698a8e716e5
IamAyushMehta <ayushmehta800006@gmail.com>
2026-07-04 17:39:08 +0530

    commit

---
+++ SelectionSort.java
@@ -0,0 +1,28 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int nums[] = { 5, 4, 1, 3, 2 };
+        selectionSort(nums);
+        printArr(nums);
+    }
+
+    public static void selectionSort(int nums[]) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[smallest] > nums[j]) {
+                    smallest = j;
+                }
+            }
+            int temp = nums[smallest];
+            nums[smallest] = nums[i];
+            nums[i] = temp;
+        }
+    }
+
+    public static void printArr(int nums[]) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
