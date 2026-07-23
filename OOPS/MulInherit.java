9ae6c72b847b5bac738bd6be653e504f36339c80
IamAyushMehta <ayushmehta800006@gmail.com>
2026-07-23 20:22:28 +0530

    commit

---
+++ OOPS/MulInherit.java
@@ -0,0 +1,25 @@
+class A {
+    A() {
+        System.out.println("in constructor of A");
+    }
+}
+
+class B extends A {
+    B() {
+        System.out.println("in constructor of B");
+    }
+}
+
+class C extends B {
+    C() {
+        System.out.println("in constructor of C");
+    }
+}
+
+public class MulInherit {
+    public static void main(String[] args) {
+        A o1 = new A();
+        B o2 = new B();
+        C o3 = new C();
+    }
+}
