24dce8f9e26a12727258650bb58b530dd9f5eece
IamAyushMehta <ayushmehta800006@gmail.com>
2026-07-24 20:23:18 +0530

    commit

--- Solution.java
+++ Solution.java
@@ -1,27 +1,19 @@
+/**
+ * Solution
+ */
 public class Solution {
 
     public static void main(String[] args) {
-        int n = 23;
+        int n = 4;
 
-        System.out.println(prime(n));
-
-        for (int i = 2; i <= n; i++) {
-            if (prime(i)) {
-                System.out.print(i + " ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
             }
-        }
-    }
-
-    public static boolean prime(int n) {
-        if (n <= 1) {
-            return false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    return false;
-                }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
+            System.out.println();
         }
-        return true;
     }
 }
