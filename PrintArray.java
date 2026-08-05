ae005b2e1dd2f5217e4023f3786eafbc1a4d75f6
IamAyushMehta <ayushmehta800006@gmail.com>
2026-08-06 00:45:06 +0530

    commit

--- PrintArray.java
+++ PrintArray.java
@@ -6,6 +6,15 @@ public class PrintArray {
         return arr;
     }
 
+    public static int[][] printArray(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+        }
+        return arr;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3 };
         printArray(arr);
--- Solution.java
+++ Solution.java
@@ -4,6 +4,17 @@
 public class Solution {
 
     public static void main(String[] args) {
-        
+        int arr[][] = {
+                { 1, 2 },
+                { 4, 5 },
+                { 6, 7 }
+        };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.println(arr[i][j] + " " + arr[i + 1][j]);
+            }
+            System.out.println();
+        }
     }
 }
