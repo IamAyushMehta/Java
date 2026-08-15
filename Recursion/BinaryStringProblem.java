b847ddcd7eb87d60a59653923bb95e8a5b28a46c
IamAyushMehta <ayushmehta800006@gmail.com>
2026-08-15 22:39:32 +0530

    commit

---
+++ Recursion/BinaryStringProblem.java
@@ -0,0 +1,19 @@
+public class BinaryStringProblem {
+    public static void binaryString(int n, int lastplace, String str) {
+        if (n == 0) {
+            System.out.println(str);
+            return;
+        }
+
+        binaryString(n - 1, 0, str + "0");
+
+        if (lastplace == 0) {
+            binaryString(n - 1, 1, str + "1");
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        binaryString(n, 0, "");
+    }
+}
